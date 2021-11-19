ef3edf967617ffdf0de38e68ef4242de74f8ebcd
ninjalol11 <76452782+ninjalol11@users.noreply.github.com>
2021-11-19 22:11:47 +0000

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,77 @@
+if game.PlaceId == 6007475062 then
+    local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+    local Window = Library.CreateLib("ARC - 32", "Serpent")
+
+    -- MAIN
+    local Main = Window:NewTab("Main")
+    local MainSection = Main:NewSection("Main")
+
+    MainSection:NewDropdown("Shop", "Buys yourself a gun", {"Mamba", "L-2 ACCESS", "CONTAGION GUN", "ARMOURY CARD", "TYPE-56 RIFLE", "AKMS", "WASR-103 B"}, function(v)
+
+    local args = {
+        [1] = v
+    }
+
+    game:GetService("ReplicatedStorage")["Credits Master"]:InvokeServer(unpack(args))
+
+        
+    end)
+
+   
+
+    -- PLAYER
+    local Player = Window:NewTab("Player")
+    local PlayerSection = Player:NewSection("Player")
+    
+    
+    PlayerSection:NewSlider("Walkspeed", "Changes the walkspeed", 250, 16, function(v)
+        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = v
+        print(v)
+    end)
+
+    PlayerSection:NewSlider("Jumppower", "Changes the jumppower", 250, 50, function(v)
+        game.Players.LocalPlayer.Character.Humanoid.JumpPower = v
+        print(v)
+
+    end)
+    
+    -- WAYPOINTS MAIN
+    
+    local Waypoints = Window:NewTab("Waypoints")
+    local Waypointsmain = Waypoints:NewSection("Waypoints")
+    
+    
+    Waypointsmain:NewButton("Work in progress", "", function()
+    print("Clicked")
+    end)
+    
+    Waypointsmain:NewButton("Work in progress", "", function()
+    print("Clicked")
+    end)
+    
+    Waypointsmain:NewButton("Work in progress", "", function()
+    print("Clicked")
+    end)
+    
+    -- SCRIPT MAIN
+    
+    local Scripts = Window:NewTab("Scripts")
+    local Scriptsmain = Scripts:NewSection("Scripts")
+    
+    Scriptsmain:NewButton("Infinite Yield", "", function()
+    print("Clicked")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
+    end)
+    
+    Scriptsmain:NewButton("RemoteSpy", "", function()
+    print("Clicked")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ninjalol11/Rspy/main/main.Lua"))()
+    end)
+    
+    Scriptsmain:NewButton("Chatspy", "", function()
+    print("Clicked")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ninjalol11/Chatspy/main/Chat"))()
+    end)
+    
+    
+end
