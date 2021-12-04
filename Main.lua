5109e00aae1329c94190f22e59f8ab27b5b1bc17
ninjalol11 <76452782+ninjalol11@users.noreply.github.com>
2021-12-04 21:18:06 +0000

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -21,7 +21,7 @@ if game.PlaceId == 6007475062 then
     print("Clicked")
     local me = game.Players.LocalPlayer.Character
     me.HumanoidRootPart.CFrame = CFrame.new(1767,52,-173)-- eye cell
-    wait(3)
+    wait(8)
     me.HumanoidRootPart.CFrame = CFrame.new(-33,3,-180)-- CDC 
     end)
     -- PLAYER
@@ -79,12 +79,7 @@ if game.PlaceId == 6007475062 then
     me.HumanoidRootPart.CFrame = CFrame.new(398,44,-564)
     end)
     
-    Waypointsmain:NewButton("CDC Control", "", function()
-    print("Clicked")
-    local me = game.Players.LocalPlayer.Character
-    me.HumanoidRootPart.CFrame = CFrame.new(167,47,-84)
-    end)
-    
+
     -- SCRIPT MAIN  -----------------------------------------------------------
     
     local Scripts = Window:NewTab("Scripts")
@@ -123,6 +118,117 @@ if v:IsA("ClickDetector") then
 fireclickdetector(v)
 end
 end
+
+elseif game.PlaceId == 7926178872 then
+    local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+    local Window = Library.CreateLib("ARC - 32, PlaceId: "..game.PlaceId, "GrapeTheme")
+
+    -- MAIN
+    local Main = Window:NewTab("Main")
+    local MainSection = Main:NewSection("Main")
+
+    MainSection:NewDropdown("Shop", "Buys yourself a gun", {"Mamba", "L-2 ACCESS", "CONTAGION GUN", "ARMOURY CARD", "TYPE-56 RIFLE", "AKMS", "WASR-103 B"}, function(v)
+
+    local args = {
+        [1] = v
+    }
+
+    game:GetService("ReplicatedStorage")["Credits Master"]:InvokeServer(unpack(args))
+    end)
+
+    MainSection:NewButton("Breach Eye", "Breches the eye you dum fuck", function()
+    print("Clicked")
+    local me = game.Players.LocalPlayer.Character
+    me.HumanoidRootPart.CFrame = CFrame.new(1767,52,-173)-- eye cell
+    wait(3)
+    me.HumanoidRootPart.CFrame = CFrame.new(-33,3,-180)-- CDC 
+    end)
+
+    -- PLAYER
+    local Player = Window:NewTab("Player")
+    local PlayerSection = Player:NewSection("Player")
+    
+    
+    PlayerSection:NewSlider("Walkspeed", "Changes the walkspeed", 250, 16, function(v)
+        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = v
+        print(v)
+    end)
+
+    PlayerSection:NewSlider("Jumppower", "Changes the jumppower", 250, 50, function(v)
+        game.Players.LocalPlayer.Character.Humanoid.JumpPower = v
+        print(v)
+
+    end)
+    
+    
+    -- WAYPOINTS MAIN
+    
+    local Waypoints = Window:NewTab("Waypoints")
+    local Waypointsmain = Waypoints:NewSection("Waypoints")
+    
+    
+    Waypointsmain:NewButton("ARMOURY", "", function()
+    print("Clicked")
+    local me = game.Players.LocalPlayer.Character
+    me.HumanoidRootPart.CFrame = CFrame.new(840,46,-202)
+
+    end)
+    
+    Waypointsmain:NewButton("Control Room", "", function()
+    print("Clicked")
+    local me = game.Players.LocalPlayer.Character
+    me.HumanoidRootPart.CFrame = CFrame.new(761,80,-380)
+    end)
+    
+    Waypointsmain:NewButton("CDC / CDCZ", "", function()
+    print("Clicked")
+    local me = game.Players.LocalPlayer.Character
+    me.HumanoidRootPart.CFrame = CFrame.new(-33,3,-180)
+    end)
+    
+    Waypointsmain:NewButton("SCP Main", "", function()
+    print("Clicked")
+    local me = game.Players.LocalPlayer.Character
+    me.HumanoidRootPart.CFrame = CFrame.new(1496,44,-177)
+    end)
+    
+    Waypointsmain:NewButton("CDC Control", "", function()
+    print("Clicked")
+    local me = game.Players.LocalPlayer.Character
+    me.HumanoidRootPart.CFrame = CFrame.new(167,47,-84)
+    end)
+    
+    Waypointsmain:NewButton("ALL GUNS", "", function()
+    print("Clicked")
+    local me = game.Players.LocalPlayer.Character
+    me.HumanoidRootPart.CFrame = CFrame.new(776,80,-166)
+    end)
+    
+    -- SCRIPT MAIN  -----------------------------------------------------------
+    local Scripts = Window:NewTab("Scripts")
+    local Scriptsmain = Scripts:NewSection("Scripts")
+    
+    Scriptsmain:NewButton("Infinite Yield", "", function()
+    print("Clicked")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
+    end)
+
+    
+    Scriptsmain:NewButton("Chatspy", "", function()
+    print("Clicked")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ninjalol11/Chatspy/main/Chat"))()
+    end)
+    -- Credits -----------------------------------------------------------
+    local Credits = Window:NewTab("Credits")
+    local Creditsmain = Credits:NewSection("Credits")
+    Creditsmain:NewButton("Exomeme#9999 For scripting", "", function()
+    end)
+    
+    Creditsmain:NewButton("Cemari77 and jerryleberry for Beta testing", "", function()
+    end)
+    
+    Creditsmain:NewButton("moos also for that.", "", function()
+    end)
 
 else
     local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
@@ -224,9 +330,9 @@ else
     Creditsmain:NewButton("Exomeme#9999 For scripting", "", function()
     end)
     
-    Creditsmain:NewButton("Rafuty and Idub for Beta testing", "", function()
+    Creditsmain:NewButton("Cemari77 and jerryleberry for Beta testing", "", function()
     end)
     
-    Creditsmain:NewButton("And well Rayzo...", "Nothing to say about him", function()
+    Creditsmain:NewButton("moos also for that.", "", function()
     end)
 end
